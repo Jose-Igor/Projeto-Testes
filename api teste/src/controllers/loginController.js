9f910820d289b5668b3b025ff453638af0eee93c
Diuari <95585201+Diuari@users.noreply.github.com>
2023-02-10 23:20:42 -0300

    atualizando verificação de senha

--- api teste/src/controllers/loginController.js
+++ api teste/src/controllers/loginController.js
@@ -1,4 +1,5 @@
 const Login = require('../models/LoginModel');
+const zxcvbn = require('zxcvbn');
 
 exports.index = (req, res) => {
   if(req.session.user) return res.render('login-logado');
@@ -7,26 +8,46 @@ exports.index = (req, res) => {
 
 exports.register = async function(req, res) {
   try {
-    const login = new Login(req.body);
-    await login.register();
-
-    if(login.errors.length > 0) {
-      req.flash('errors', login.errors);
-      req.session.save(function() {
-        return res.redirect('back');
-      });
-      return;
-    }
-
-    req.flash('success', 'Seu usuário foi criado com sucesso.');
+  const password = req.body.password;
+  const passwordStrength = zxcvbn(password);
+  if (passwordStrength.score < 3) {
+    req.flash('errors', ['A senha deve conter números, letras e caracteres especiais.']);
     req.session.save(function() {
       return res.redirect('back');
     });
-  } catch(e) {
+    return;
+  }
+  
+  if (!/\d/.test(password) || !/[a-zA-Z]/.test(password) || !/[!@#\$%\^&\*]/.test(password)) {
+    req.flash('errors', ['A senha deve conter números, letras e caracteres especiais.']);
+    req.session.save(function() {
+      return res.redirect('back');
+    });
+    return;
+  }
+  
+  const login = new Login(req.body);
+  await login.register();
+  
+  if (login.errors.length > 0) {
+    req.flash('errors', login.errors);
+    req.session.save(function() {
+      return res.redirect('back');
+    });
+    return;
+  }
+  
+  req.flash('success', 'Seu usuário foi criado com sucesso.');
+  req.session.save(function() {
+    return res.redirect('back');
+  });}
+
+  catch (e) {
     console.log(e);
     return res.render('404');
-  }
+    }
 };
+  
 
 exports.login = async function(req, res) {
   try {
